08af8d0900d7a1eabf25d2caa2fc36e1c10dd5dd
Stanley Louis <stanlouis@msn.com>
2019-07-23 11:47:08 -0400

    Add SignIn, FormInput and CustomButton components

---
+++ src/components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+export default class SignIn extends Component {
+  state = { email: '', password: '' };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            required
+            handleChange={this.handleChange}
+            label="email"
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            required
+            handleChange={this.handleChange}
+            label="password"
+          />
+
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
